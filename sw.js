5ca5923f9c2c9d16def07edad3c581f6a926ed94
3330589157-debug <3330589157@qq.com>
2026-09-20 21:45:40 +0800

    deploy sw.js

---
+++ sw.js
@@ -0,0 +1,29 @@
+var CACHE="fitness-v1";
+var ASSETS=["./","./index.html","./健身工作台.html","./manifest.webmanifest","./icon-192.png","./icon-512.png","./apple-touch-icon.png"];
+self.addEventListener("install",function(e){
+  e.waitUntil(caches.open(CACHE).then(function(c){ return c.addAll(ASSETS); }));
+  self.skipWaiting();
+});
+self.addEventListener("activate",function(e){
+  e.waitUntil(caches.keys().then(function(keys){
+    return Promise.all(keys.filter(function(k){ return k!==CACHE; }).map(function(k){ return caches.delete(k); }));
+  }));
+  self.clients.claim();
+});
+self.addEventListener("fetch",function(e){
+  var url=new URL(e.request.url);
+  if(e.request.method!=="GET"||url.origin!==location.origin) return;
+  e.respondWith(
+    fetch(e.request).then(function(res){
+      if(res&&res.status===200){
+        var clone=res.clone();
+        caches.open(CACHE).then(function(c){ c.put(e.request,clone); });
+      }
+      return res;
+    }).catch(function(){
+      return caches.match(e.request).then(function(hit){
+        return hit||caches.match("./健身工作台.html");
+      });
+    })
+  );
+});
